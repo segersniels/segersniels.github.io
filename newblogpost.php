50e5bd36cc2932f1221f1b58976c45fa58dfa29a
Niels Segers <segers.n@hotmail.com>
2016-10-01 10:52:08 +0200

    new post

---
+++ newblogpost.php
@@ -0,0 +1,32 @@
+<?php
+// get title of post
+echo "Title of new post:\n";
+$stdin = fopen('php://stdin', 'r');
+$title = fgetc($stdin);
+// get content for post
+echo "Content of new post:\n";
+$stdin = fopen('php://stdin', 'r');
+$content = fgetc($stdin);
+// transform command line input into new blog post in html file
+$data = file('index.html'); // reads an array of lines
+function replace_a_line($data) {
+   if (stristr($data, '<div class="post-preview">')) {
+     return "<div class='post-preview'>
+     			<div class='post'>
+                	<h2 class='post-title'>" 
+                		. $title . 
+                	"</h2>
+                	<h3 class='post-subtitle'>"
+                		. $content . 
+                	"</h3>
+            	</div>
+            	<hr>";
+   }
+   return $data;
+}
+$data = array_map('replace_a_line',$data);
+file_put_contents('index.html', implode('', $data));
+exec("git add .");
+exec("git commit -m 'new post'");
+exec("git push");
+?>
