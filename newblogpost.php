abc5abc865653367e2d8fa6bbd4aff2ff091e468
Niels Segers <segers.n@hotmail.com>
2016-10-01 11:15:18 +0200

    new post

--- newblogpost.php
+++ newblogpost.php
@@ -1,33 +1,25 @@
 <?php
 // get title of post
 echo "Title of new post: ";
-$handle_title = fopen ("php://stdin","r");
+$handle_title = fopen("php://stdin","r");
 $title = fgets($handle_title);
+
 // get content for post
 echo "Content of new post: ";
-$handle_content = fopen ("php://stdin","r");
+$handle_content = fopen("php://stdin","r");
 $content = fgets($handle_content);
-// transform command line input into new blog post in html file
-$data = file('index.html');
-function replace_a_line($data) {
-   if (stristr($data, '<div class="post-preview">')) {
-     return "<div class='post-preview'>
-     			<div class='post'>
-                	<h2 class='post-title'>" 
-                		. $title . 
-                	"</h2>
-                	<h3 class='post-subtitle'>"
-                		. $content . 
-                	"</h3>
-            	</div>
-            	<hr>";
-   }
-   return $data;
-}
-$data = array_map('replace_a_line',$data);
-file_put_contents('index.html', implode('', $data));
 
-// check if new post is correct, if so push it to git
+// transform command line input into new blog post in html file
+
+
+//test
+
+$path_to_file = 'index.html';
+$file_contents = file_get_contents($path_to_file);
+$file_contents = str_replace("<div class='post-preview'>","<div class='post-preview'><div class='post'><h2 class='post-title'>" . $title . "</h2><h3 class='post-subtitle'>". $content . "</h3></div><hr>",$file_contents);
+file_put_contents($path_to_file,$file_contents);
+
+// check if new post is correct, if so push it to git, if not abort
 echo "\nYour current post looks like this:\n\n";
 echo "Title: " . $title;
 echo "Content: " . $content."\n\n";
