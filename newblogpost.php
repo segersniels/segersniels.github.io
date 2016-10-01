60fb8c098fb6f796fbd6c541e232a94462a442d5
Niels Segers <segers.n@hotmail.com>
2016-10-01 11:05:39 +0200

    new post

--- newblogpost.php
+++ newblogpost.php
@@ -1,12 +1,12 @@
 <?php
 // get title of post
 echo "Title of new post: ";
-$stdin = fopen('php://stdin', 'r');
-$title = fgetc($stdin);
+$handle_title = fopen ("php://stdin","r");
+$title = fgets($handle_title);
 // get content for post
 echo "Content of new post: ";
-$stdin = fopen('php://stdin', 'r');
-$content = fgetc($stdin);
+$handle_content = fopen ("php://stdin","r");
+$content = fgets($handle_content);
 // transform command line input into new blog post in html file
 $data = file('index.html');
 function replace_a_line($data) {
@@ -26,7 +26,22 @@ function replace_a_line($data) {
 }
 $data = array_map('replace_a_line',$data);
 file_put_contents('index.html', implode('', $data));
-exec("git add .");
-exec("git commit -m 'new post'");
-exec("git push");
+
+// check if new post is correct, if so push it to git
+echo "\nYour current post looks like this:\n\n";
+echo "Title: " . $title;
+echo "Content: " . $content."\n\n";
+echo "Does this look correct?  Type 'yes' to continue: ";
+$handle = fopen ("php://stdin","r");
+$line = fgets($handle);
+if(trim($line) == 'yes'){
+	exec("git add .");
+	exec("git commit -m 'new post'");
+	exec("git push");
+}
+else{
+	echo "\n";
+    echo "ABORTING!\n";
+    exit;
+}
 ?>
