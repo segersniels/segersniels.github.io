363d8c46d0df5dea05185b3f90ff8f100dd4bbcf
Niels Segers <segers.n@hotmail.com>
2016-10-14 20:39:11 +0200

    new post

--- new-item.php
+++ new-item.php
@@ -49,12 +49,6 @@ if($blogpost == true){
 	$handle_content = fopen("php://stdin","r");
 	$content = fgets($handle_content);
 
-// transform command line input into new blog post in html file
-	$path_to_file = './index.html';
-	$file_contents = file_get_contents($path_to_file);
-	$file_contents = str_replace('<div class="post-preview">','<div class="post-preview">' . "\n\t\t\t\t\t\t" . '<div class="post">'. "\n\t\t\t\t\t\t\t" .'<h2 class="post-title">'. "\n\t\t\t\t\t\t\t\t" . $title . "\t\t\t\t\t\t\t" . '</h2>'. "\n\t\t\t\t\t\t\t" .'<h3 class="post-subtitle">'. "\n\t\t\t\t\t\t\t\t" . $content . "\t\t\t\t\t\t\t" . '</h3>'. "\n\t\t\t\t\t\t" .'</div>'. "\n\t\t\t\t\t\t" .'<hr>',$file_contents);
-	file_put_contents($path_to_file,$file_contents);
-
 // check if new post is correct, if so push it to git, if not abort
 	echo "\nYour current post looks like this:\n\n";
 	echo "Title: " . $title;
@@ -62,7 +56,13 @@ if($blogpost == true){
 	echo "Does this look correct?  Type 'yes' to continue: ";
 	$handle = fopen ("php://stdin","r");
 	$line = fgets($handle);
-	if(trim($line) == 'yes'){
+	if(trim($line) == 'yes'){	
+		// transform command line input into new blog post in html file
+		$path_to_file = './index.html';
+		$file_contents = file_get_contents($path_to_file);
+		$file_contents = str_replace('<div class="post-preview">','<div class="post-preview">' . "\n\t\t\t\t\t\t" . '<div class="post">'. "\n\t\t\t\t\t\t\t" .'<h2 class="post-title">'. "\n\t\t	\t\t\t\t\t\t" . $title . "\t\t\t\t\t\t\t" . '</h2>'. "\n\t\t\t\t\t\t\t" .'<h3 class="post-subtitle">'. "\n\t\t\t\t\t\t\t\t" . $content . "\t\t\t\t\t\t\t" . '</h3>'. "\n\t\t\t\t\t\t"	 .'</div>'. "\n\t\t\t\t\t\t" .'<hr>',$file_contents);
+		file_put_contents($path_to_file,$file_contents);
+		// push to git
 		exec("git add .");
 		exec("git commit -m 'new post'");
 		exec("git push");
@@ -94,12 +94,6 @@ if($assignment == true){
 	$handle_link = fopen("php://stdin","r");
 	$link = rtrim(fgets($handle_link),"\r\n"); //Remove the newline from the end of the input
 	
-	// transform command line input into new assignment post in html file
-	$path_to_file = 'html/school.html';
-	$file_contents = file_get_contents($path_to_file);
-	$file_contents = str_replace('<h2 class="post-title">I3Talent</h2>','<h2 class="post-title">I3Talent</h2>' . "\n\t\t\t\t\t\t" . '<p><b>Assignment: '. $title .'</b></p>'. "\n\t\t\t\t\t\t" . 	'<iframe width="100%" height="1000px" src="' . $link . '" type="application/pdf"></iframe>',$file_contents);
-	file_put_contents($path_to_file,$file_contents);
-	
 	// check if new assignment is correct, if so push it to git, if not abort
 	echo "\nNew assignment:\n\n";
 	echo "Title: " . $title . "\n";
@@ -108,6 +102,12 @@ if($assignment == true){
 	$handle = fopen ("php://stdin","r");
 	$line = fgets($handle);
 	if(trim($line) == 'yes'){
+		// transform command line input into new assignment post in html file
+		$path_to_file = 'html/school.html';
+		$file_contents = file_get_contents($path_to_file);
+		$file_contents = str_replace('<h2 class="post-title">I3Talent</h2>','<h2 class="post-title">I3Talent</h2>' . "\n\t\t\t\t\t\t" . '<p><b>Assignment: '. $title .'</b></p>'. "\n\t\t\t\t\t\t	" . 	'<iframe width="100%" height="1000px" src="' . $link . '" type="application/pdf"></iframe>',$file_contents);
+		file_put_contents($path_to_file,$file_contents);
+		// push to git
 		exec("git add .");
 		exec("git commit -m 'new assignment'");
 		exec("git push");
