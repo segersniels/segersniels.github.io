0b5670de3dada4f7ad52f724f3c4b028657c7c3b
Niels Segers <segers.n@hotmail.com>
2016-10-14 19:57:13 +0200

    new assignment

--- newschoolassignment.php
+++ newschoolassignment.php
@@ -14,7 +14,7 @@ $link = fgets($handle_link);
 // transform command line input into new assignment post in html file
 $path_to_file = 'html/school.html';
 $file_contents = file_get_contents($path_to_file);
-$file_contents = str_replace('<h2 class="post-title">I3Talent</h2>','<h2 class="post-title">I3Talent</h2>' . "\n\t\t\t\t\t\t" . '<p><b>Assignment: '. $title . '</b></p>'. "\n\t\t\t\t\t\t" . '<iframe width="100%" height="1000px" src="' . $link . '" type="application/pdf"></iframe>',$file_contents);
+$file_contents = str_replace('<h2 class="post-title">I3Talent</h2>','<h2 class="post-title">I3Talent</h2>' . "\n\t\t\t\t\t\t" . '<p><b>Assignment: '. $title .'</b></p>'. "\n\t\t\t\t\t\t" . '<iframe width="100%" height="1000px" src="' . $link . '" type="application/pdf"></iframe>',$file_contents);
 file_put_contents($path_to_file,$file_contents);
 
 // check if new assignment is correct, if so push it to git, if not abort
