20dfbd6fa67eea762c45b047f2081aa6ac8d862a
Niels Segers <segers.n@hotmail.com>
2016-10-14 20:01:47 +0200

    new assignment

--- newschoolassignment.php
+++ newschoolassignment.php
@@ -1,15 +1,16 @@
 <?php
 // get title of assignment
+echo "\n";
 echo "Title of new assignment: ";
 $handle_title = null;
 $handle_title = fopen("php://stdin","r");
-$title = fgets($handle_title);
+$title = rtrim(fgets($handle_title),"\r\n"); //Remove the newline/whitespace from the end of the input
 
 // get link for assignment
 echo "URL link to assignment pdf file: ";
 $handle_link = null;
 $handle_link = fopen("php://stdin","r");
-$link = fgets($handle_link);
+$link = rtrim(fgets($handle_link),"\r\n"); //Remove the newline from the end of the input
 
 // transform command line input into new assignment post in html file
 $path_to_file = 'html/school.html';
@@ -19,7 +20,7 @@ file_put_contents($path_to_file,$file_contents);
 
 // check if new assignment is correct, if so push it to git, if not abort
 echo "\nNew assignment:\n\n";
-echo "Title: " . $title;
+echo "Title: " . $title . "\n";
 echo "URL: " . $link."\n\n";
 echo "Does this look correct?  Type 'yes' to continue: ";
 $handle = fopen ("php://stdin","r");
