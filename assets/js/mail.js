82bef782cec1a06c97c410c0b1b978cd1c043957
Niels Segers <segers.n@hotmail.com>
2017-05-15 22:02:30 +0200

    update

---
+++ assets/js/mail.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+    // process the form
+    $('form').submit(function(event) {
+
+        // get the form data
+        // there are many ways to get this data using jQuery (you can use the class or id also)
+        var formData = {
+            'name'              : $('input[name=name]').val(),
+            'email'             : $('input[name=email]').val(),
+            'subject'           : $('input[name=subject]').val(),
+            'text'              : $('input[name=message]').val()
+        };
+
+        // process the form
+        $.ajax({
+            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url         : 'https://veuwyhpsk9.execute-api.eu-west-1.amazonaws.com/dev/mail/post', // the url where we want to POST
+            data        : formData, // our data object
+            dataType    : 'json', // what type of data do we expect back from the server
+            encode          : true
+        })
+            // using the done promise callback
+            .done(function(data) {
+
+                // log data to the console so we can see
+                console.log(data); 
+
+                // here we will handle errors and validation messages
+            });
+
+        // stop the form from submitting the normal way and refreshing the page
+        event.preventDefault();
+    });
+
+});
