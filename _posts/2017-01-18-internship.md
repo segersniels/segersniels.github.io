8bd8d5ef43496ea936b735e72d2d2f1f820be7fd
segersniels <segers.n@hotmail.com>
2018-02-21 23:37:30 +0100

    update

--- _posts/2017-01-18-internship.md
+++ _posts/2017-01-18-internship.md
@@ -1,6 +1,6 @@
 ---
 layout: post
-title: End of internship
+title: Internship
 ---
 
 Exciting times ahead! I got accepted as an intern at Small Town Heroes. I will be working on the Operations team with Docker and AWS.
---
+++ _site/new-post.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Variables
+TIME=$(date +'%Y-%m-%d')
+
+# User input
+echo "Title?"
+read title
+
+filename=$(echo $title |tr '[:upper:]' '[:lower:]' |tr '\ ' '-')
+FILE="_posts/$TIME-$filename.md"
+
+cat > "$FILE" <<EOF
+---
+layout: post
+title: ${title}
+---
+
+EOF
+
+echo "New post created at '$FILE'"
+
+exec vi "$FILE"
